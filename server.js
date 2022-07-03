dfb2456a97709adb914237d682a33488f7ffcdfc
Oxrod <oxrodo@gmail.com>
2022-07-03 18:07:20 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
+// Check if not in production environment
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+// Init
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+// Routers
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
+// Database Connection
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true })
 const db = mongoose.connection;
+// DEBUG
 db.on('error', error => console.log(error));
 db.once('open', () => console.log("Connected to Mongoose"));
 
+// Router linking
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
 app.listen(process.env.PORT || 3000)
